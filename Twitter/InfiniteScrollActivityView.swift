cfefe7787eb86348364e47a774b98f9c783b4b2b
Karlygash Zhuginissova <karlygash1510@gmail.com>
2016-02-21 23:32:34 -0500

    first commit

---
+++ Twitter/InfiniteScrollActivityView.swift
@@ -0,0 +1,21 @@
+//
+//  InfiniteScrollActivityView.swift
+//  Twitter
+//
+//  Created by Karlygash Zhuginissova on 2/21/16.
+//  Copyright © 2016 Karlygash Zhuginissova. All rights reserved.
+//
+
+import UIKit
+
+class InfiniteScrollActivityView: UIActivityIndicatorView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Twitter/Media.swift
@@ -0,0 +1,13 @@
+//
+//  Media.swift
+//  Twitter
+//
+//  Created by Karlygash Zhuginissova on 2/16/16.
+//  Copyright © 2016 Karlygash Zhuginissova. All rights reserved.
+//
+
+import UIKit
+
+class Media: NSObject {
+
+}
